28b5f7c5eb28aaceddc5f579593684a766a1c866
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-28 17:03:14 +0000

    basics set

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,34 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	let apiUrl = "https://swapi.dev/api/"
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			planets: [],
+			starships: [],
+			favorites: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			getCharacters: () => {
+				fetch(apiUrl + "people")
+				.then(resp => resp.json())
+				.then(data => setStore({characters:data.results}))
+				.catch(error => console.log(error))
 			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+			getPlantets: () => {
+				fetch(apiUrl + "planets")
+				.then(resp => resp.json())
+				.then(data => setStore({planets:data.results}))
+				.catch(error => console.log(error))
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			getStarships: () => {
+				fetch(apiUrl + "starships")
+				.then(resp => resp.json())
+				.then(data => setStore({starships:data.results}))
+				.catch(error => console.log(error))
 			}
 		}
 	};
 };
+
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,21 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import { Card } from "../component/card";
 
-export const Home = () => (
+
+
+export const Home = () => {
+	const {store, actions} = useContext(Context)
+	return (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<h1>Characters</h1>
+		<div className="d-flex">
+			{store.characters.map((item, index) => {
+				return (
+					< Card item = {item} index = {index} />
+				)
+			})}
+		</div>
 	</div>
-);
+)};
