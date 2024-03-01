3769ccef7cdc79557d99a87a684b0ef0873081f5
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-03-01 06:34:33 +0000

    added function so heart stays red when in favorites tab

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -6,7 +6,7 @@ export const Card = ({item, index, category}) => {
     const GUIDE_URL = "https://starwars-visualguide.com/assets/img/";
     let EMPTY_PIC_URL;
     const [imageError, setImageError] = useState(false);
-    const { actions } = useContext(Context);
+    const { store, actions } = useContext(Context);
 
     const handleImageError = () => {
         setImageError(true);
@@ -23,6 +23,7 @@ export const Card = ({item, index, category}) => {
     const addToFavorites = () => {
         actions.getFavorites({ name: item.name, index, category });
     };
+    const isFavorite = store.favorites.some(fav => fav.name === item.name && fav.category === category);
 
     return (
         <div className="card">
@@ -60,7 +61,7 @@ export const Card = ({item, index, category}) => {
                         <button type="button" className="btn btn-primary">Learn more!</button>
                     </Link>
                     <button type="button" className="btn btn-outline-warning btn-heart" onClick={addToFavorites}>
-                        <i className="fa-solid fa-heart heartBtn"></i>
+                        <i className="fa-solid fa-heart heartBtn" style={{ color: isFavorite ? '#cc0020' : '#ffc107' }}></i>
                     </button>
                 </div>
             </div>
